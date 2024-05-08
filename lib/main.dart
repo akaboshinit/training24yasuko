8a52a9eb99666e54e96b5db429ba7c8927f51437
akaboshinit <akaboshinit@gmail.com>
2024-05-08 15:22:45 +0900

    audio再生を実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setUrl('assets/audio/yasuko_kafun.mp3');
+                  player.play();
+                },
+                child: const Text('ｶﾌﾝ----!'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setUrl('assets/audio/yasuko_hi_long.mp3');
+                  player.play();
+                },
+                child: const Text('longﾊｲ----!'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setUrl('assets/audio/yasuko_hi_short.mp3');
+                  player.play();
+                },
+                child: const Text('ﾊｲ----!'),
+              ),
+            ],
+          ),
         ),
       ),
     );
