3a3ee4ba08211146b9d37ca30daccf54872e46d8
akaboshinit <akaboshinit@gmail.com>
2024-05-08 17:15:00 +0900

    ogpの設定 不要なファイルを整理

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "やす子は一歩も引かない",
       home: Scaffold(
         body: SizedBox(
           width: double.infinity,
