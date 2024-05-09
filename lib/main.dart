6435599a52fa149ba7552c03a5e63c93906bf969
akaboshinit <akaboshinit@gmail.com>
2024-05-09 16:53:08 +0900

    レイアウトを設定

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:training24yasuko/firebase_options.dart';
-import 'package:training24yasuko/model.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(
@@ -16,8 +15,8 @@ Future<void> main() async {
 
 enum AudioType {
   kafun,
-  hiShort,
-  hiLong;
+  hiShort;
+  // hiLong;
 
   String get text {
     switch (this) {
@@ -25,8 +24,8 @@ enum AudioType {
         return "花粉";
       case AudioType.hiShort:
         return "ﾊｲ----!";
-      case AudioType.hiLong:
-        return "ﾊｲ----!";
+      // case AudioType.hiLong:
+      //   return "ﾊｲ----!";
     }
   }
 
@@ -36,8 +35,8 @@ enum AudioType {
         return "https://firebasestorage.googleapis.com/v0/b/training24yasuko.appspot.com/o/yasuko_kafun.mp3?alt=media";
       case AudioType.hiShort:
         return "https://firebasestorage.googleapis.com/v0/b/training24yasuko.appspot.com/o/yasuko_hi_short.mp3?alt=media";
-      case AudioType.hiLong:
-        return "https://firebasestorage.googleapis.com/v0/b/training24yasuko.appspot.com/o/yasuko_hi_long.mp3?alt=media";
+      // case AudioType.hiLong:
+      //   return "https://firebasestorage.googleapis.com/v0/b/training24yasuko.appspot.com/o/yasuko_hi_long.mp3?alt=media";
     }
   }
 }
@@ -47,106 +46,287 @@ class MainApp extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
-    final yasukoCollection = db.collection("yasukoCount");
-    final player = AudioPlayer();
+    return const MaterialApp(
+        title: "やす子は一歩も引かない", debugShowCheckedModeBanner: false, home: App());
+  }
+}
+
+final db = FirebaseFirestore.instance;
+final yasukoCollection = db.collection("yasukoCount");
+final player = AudioPlayer();
+
+class App extends HookWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // final hiCountStream =
+    //     useStream(yasukoCollection.doc(AudioType.hiShort.name).snapshots());
+    // final kafunCountStream =
+    //     useStream(yasukoCollection.doc(AudioType.kafun.name).snapshots());
+
+    final hiCount = useState(0);
+    final kafunCount = useState(0);
 
     useEffect(() {
+      player.setVolume(1.0);
       for (var url in AudioType.values) {
         player.setAudioSource(
           AudioSource.uri(Uri.parse(url.getUrl)),
         );
       }
+
       return null;
     }, []);
 
-    return MaterialApp(
-      title: "やす子は一歩も引かない",
-      home: Scaffold(
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AudioBox(
-                type: AudioType.kafun,
-                player: player,
-                yasukoCollection: yasukoCollection,
-              ),
-              const SizedBox(height: 16.0),
-              AudioBox(
-                type: AudioType.hiShort,
-                player: player,
-                yasukoCollection: yasukoCollection,
-              ),
-              // const SizedBox(height: 16.0),
-              // AudioBox(
-              //   type: AudioType.hiLong,
-              //   player: player,
-              //   yasukoCollection: yasukoCollection,
-              // ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+    // useEffect(() {
+    //   if (hiCountStream.data?.data()!["count"] != null &&
+    //       hiCountStream.data?.data()!["count"] != 1) {
+    //     // hiCount.value = hiCountStream.data?.data()!["count"] ?? 0;
+    //   }
+    //   return null;
+    // }, [hiCountStream.data]);
 
-class AudioBox extends HookWidget {
-  const AudioBox(
-      {super.key,
-      required this.type,
-      required this.player,
-      required this.yasukoCollection});
-
-  final AudioType type;
-  final AudioPlayer player;
-  final CollectionReference yasukoCollection;
-
-  @override
-  Widget build(BuildContext context) {
-    Future<void> startAudio(AudioType type) async {
-      final storageUrl = type.getUrl;
-      await player.setUrl(storageUrl);
-      player.play();
-    }
+    // useEffect(() {
+    //   if (kafunCountStream.data?.data()!["count"] != null &&
+    //       kafunCountStream.data?.data()!["count"] != 1) {
+    //     // kafunCount.value = kafunCountStream.data?.data()!["count"] ?? 0;
+    //   }
+    //   return null;
+    // }, [kafunCountStream.data]);
 
     Future<void> tap({
-      required VoidCallback onIncrement,
+      required AudioType type,
     }) async {
+      final storageUrl = type.getUrl;
+
+      AudioPlayer tmpPlayer = player;
+      if (type == AudioType.hiShort) {
+        tmpPlayer = AudioPlayer();
+      } else {
+        await tmpPlayer.stop();
+      }
+
+      await tmpPlayer.setUrl(storageUrl);
+      tmpPlayer.play();
       await yasukoCollection.doc(type.name).set({
         "count": FieldValue.increment(1),
       }, SetOptions(merge: true));
-      onIncrement();
     }
 
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: () async {
-            await startAudio(type);
-            await tap(onIncrement: () {});
-          },
-          child: Text(type.text),
-        ),
-        StreamBuilder(
-          stream: yasukoCollection.doc(type.name).snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.error != null ||
-                !snapshot.hasData ||
-                snapshot.data == null) {
-              return const CircularProgressIndicator();
-            }
+    final maxWidth = 400.toDouble();
 
-            final counter = Counter.fromFirestore(snapshot.data?.data() ?? {});
-            final count = counter.count;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: SizedBox(
+          width: maxWidth,
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.black,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  width: maxWidth,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.network(
+                        "cover.png",
+                        fit: BoxFit.contain,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        height: 390,
+                        width: maxWidth,
+                        child: Container(
+                          foregroundDecoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.black,
+                                Colors.transparent,
+                              ],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              stops: [0.6, 0],
+                            ),
+                          ),
+                          child: const SizedBox(
+                            width: double.infinity,
+                            height: 900,
+                          ),
+                        ),
+                      ),
+                      // const Positioned(
+                      //   top: 400,
+                      //   child:
+                      // )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      const Text(
+                        "みんなで連打！！",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.bold,
+                          shadows: <Shadow>[
+                            Shadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 1.0,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                            Shadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 2.0,
+                              color: Color.fromARGB(255, 10, 144, 50),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          await tap(
+                            type: AudioType.kafun,
+                          );
+                        },
+                        child: Image.network(
+                          "button2.png",
+                          width: 300,
+                        ),
+                      ),
+                      StreamBuilder(
+                        stream: yasukoCollection.doc("kafun").snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.error != null ||
+                              !snapshot.hasData ||
+                              snapshot.data == null) {
+                            return const CircularProgressIndicator();
+                          }
 
-            return Text("$count");
-          },
+                          final count = snapshot.data?.data()!["count"];
+
+                          return RichText(
+                            text: TextSpan(children: [
+                              const TextSpan(
+                                text: "連続",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const WidgetSpan(
+                                  alignment: PlaceholderAlignment.baseline,
+                                  baseline: TextBaseline.alphabetic,
+                                  child: SizedBox(width: 10)),
+                              TextSpan(
+                                text: count.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 38,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const WidgetSpan(
+                                  alignment: PlaceholderAlignment.baseline,
+                                  baseline: TextBaseline.alphabetic,
+                                  child: SizedBox(width: 10)),
+                              const TextSpan(
+                                text: "回",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ]),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 12),
+                      GestureDetector(
+                        onTap: () async {
+                          await tap(
+                            type: AudioType.hiShort,
+                          );
+                        },
+                        child: Image.network(
+                          "button1.png",
+                          width: 300,
+                        ),
+                      ),
+                      StreamBuilder(
+                        stream: yasukoCollection.doc("hiShort").snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.error != null ||
+                              !snapshot.hasData ||
+                              snapshot.data == null) {
+                            return const CircularProgressIndicator();
+                          }
+
+                          final count = snapshot.data?.data()!["count"];
+
+                          return RichText(
+                            text: TextSpan(children: [
+                              const TextSpan(
+                                text: "連続",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const WidgetSpan(
+                                  alignment: PlaceholderAlignment.baseline,
+                                  baseline: TextBaseline.alphabetic,
+                                  child: SizedBox(width: 10)),
+                              TextSpan(
+                                text: count.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 38,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const WidgetSpan(
+                                  alignment: PlaceholderAlignment.baseline,
+                                  baseline: TextBaseline.alphabetic,
+                                  child: SizedBox(width: 10)),
+                              const TextSpan(
+                                text: "回",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ]),
+                          );
+                        },
+                      ),
+                      const SizedBox(
+                        height: 28,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-      ],
+      ),
     );
   }
 }
